1124ed3dcb81cdaa55daa60d765471f52a06498b
Judith Ogar <judithmanyo2@gmail.com>
2022-02-10 20:32:20 +0100

    xabeez

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //javascript code for menu bar
-var toggleButton = document.getElementsByClassName('toggle-button')[0]
-var navbarLinks = document.getElementsByClassName('navbar-links')[0]
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 //take the navbar links and acces the different classes on it(use classlist)then toggle the active class(meaning if the active class exist it will add it and if it dosesnt exist it will remove it)//
 
@@ -9,31 +9,33 @@ toggleButton.addEventListener('click', () => {
 })
 
 //javascript code for add to cart button
-var addToCart = document.getElementsByClassName('buy');
-var mainContainer = document.getElementsByTagName("tbody")[0];
+ const cartContainer = document.getElementsByTagName("tbody")[0];    
+              
+const addToCart = document.getElementsByClassName('buy');
 
 for (var i =0; i< addToCart.length; i++){
 addToCart[i].addEventListener('click', addItem)
 }
 function addItem(event){
- let btn = event.target;
- let btn_parent = btn.parentElement
-  let itemName = btn_parent.children[2].innerText;
-  let itemImage = btn_parent.children[0].src;
-  let itemPrice = btn_parent.children[3].innerHTML;
+  let btn = event.target;
+  let btnParent = btn.parentElement
+  let itemName = btnParent.children[2].innerText;
+  let itemImage = btnParent.children[0].src;
+  let itemPrice = btnParent.children[3].innerText;
 
+  let itemContainer = document.createElement('tr');
+  itemContainer.innerHTML = `
+    <td><img class="item-image" src=${itemImage} width="40" alt=""></td>
+    <td class = "item-name">${itemName}</h3>
+    </td>
+    <td class="item-price"><h3>${itemPrice}</h3></td>
+    <td><input type = 'number' class = 'num' value = '1'></td>
+    <td class="total-price"><h3>${itemPrice}</h3></td>
+    <td><button class="delete-button" type="button">Remove</button></td>
+`
+console.log(itemContainer)
 
-var itemContainer = document.createElement('tr');
-itemContainer.innerHTML = `
-                    <td><input class="uk-checkbox" type="checkbox"></td>
-                    <td><img class="image" width="40" alt="">${itemImage}</td>
-                    <td class="uk-table-link">
-                        <h3 class = "name">${itemName}</h3>
-                    </td>
-                    <td class="uk-text-truncate item-price"><h3>${itemPrice}</h3></td>
-                    <td><input type = 'number' class = 'num' value = '1'></td>
-                    <td class="uk-text-truncate total-price"><h3>$100</h3></td>
-                    <td><button class="uk-button uk-button-danger" type="button">Remove</button></td>`
-                    
-mainContainer.appendChild(itemContainer);
+   cartContainer.appendChild(itemContainer);
+
 }
+/*"Uncaught TypeError: cartContent.appendChild is not a function at HTMLButtonElement.addItem " how can i solve this*/
